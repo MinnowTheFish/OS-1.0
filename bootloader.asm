761c4fc31fff80933eaedab22c83c9dc379900dd
BeefStew <34359784+BeefStew34@users.noreply.github.com>
2020-09-05 15:07:59 +1200

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,12 +1,12 @@
 [bits 16]
 [org 0x7c00]
 
-global start
-
 start:
-    xor ax,ax
-    mov ds,ax
-    mov es,ax
+    xor ax,ax 
+    mov ds,ax           
+    mov es,ax           
+    mov bx,0x8000
     
+    ret
     times 510-($-$$) db 0
     dw 0xAA55
